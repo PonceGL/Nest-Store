fda96cb2e298b22ebba8b05b4b382908b9a40bc0
“PonceGL” <poncianogl@hotmail.com>
2024-03-28 11:21:38 -0600

    feat(develop): create Product DTO

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const apiPrefix = process.env.API_PREFIX;
+  const apiVersion = process.env.API_VERSION;
+  app.setGlobalPrefix(`${apiPrefix}/${apiVersion}`);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
