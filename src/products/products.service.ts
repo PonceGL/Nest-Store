96451f4aa80f21c6a1ade1693b5115cfbf1fd9ff
“PonceGL” <poncianogl@hotmail.com>
2024-03-28 11:56:11 -0600

    feat(develop): error handling

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,9 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -7,6 +12,8 @@ import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductsService {
+  private readonly logger = new Logger('ProductsService');
+
   constructor(
     @InjectRepository(Product)
     private readonly productRepository: Repository<Product>,
@@ -24,10 +31,7 @@ export class ProductsService {
         product,
       };
     } catch (error) {
-      console.log('====================================');
-      console.log(error);
-      console.log('====================================');
-      throw new InternalServerErrorException('Error creating product');
+      this.handleDBException(error);
     }
   }
 
@@ -45,5 +49,15 @@ export class ProductsService {
 
   remove(id: number) {
     return `This action removes a #${id} product`;
+  }
+
+  private handleDBException(error: any) {
+    if (error.code === '23505') {
+      throw new BadRequestException(error.detail);
+    }
+    this.logger.error(error);
+    throw new InternalServerErrorException(
+      'Unexpected error occurred, check server logs',
+    );
   }
 }
