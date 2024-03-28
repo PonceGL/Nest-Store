a08ed2a7721e4d271e7abffc021170bc03e0e910
“PonceGL” <poncianogl@hotmail.com>
2024-03-28 11:43:56 -0600

    feat(develop): insert with TypeORM

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,11 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+  ) {}
+
+  async create(createProductDto: CreateProductDto) {
+    const successMessage = 'This action adds a new product';
+
+    try {
+      const product = this.productRepository.create(createProductDto);
+      await this.productRepository.save(product);
+      return {
+        success: true,
+        message: successMessage,
+        product,
+      };
+    } catch (error) {
+      console.log('====================================');
+      console.log(error);
+      console.log('====================================');
+      throw new InternalServerErrorException('Error creating product');
+    }
   }
 
   findAll() {
